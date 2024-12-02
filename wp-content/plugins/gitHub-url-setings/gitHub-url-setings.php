60423c43f51a9fdc0ee5ec5e4ae29239569ed0fd
Lucas Kennde <lucaskennde@gmail.com>
2024-12-02 18:06:50 -0300

    Created Register settings

--- wp-content/plugins/gitHub-url-setings/gitHub-url-setings.php
+++ wp-content/plugins/gitHub-url-setings/gitHub-url-setings.php
@@ -23,6 +23,7 @@ class GitHubUrlSettings
     public function __construct()
     {
         add_action('admin_menu', [$this, 'add_admin_page']);
+        add_action('admin_init', [$this, 'register_settings']); // Registra as opções de configurações
     }
 
     // Funçõa para adicionar a página ao menu de configurações
@@ -40,9 +41,40 @@ class GitHubUrlSettings
     {
 ?>
         <div class="wrap">
-            <h1>Configurar GitHub URL</h1>
+            <h1>Configurar URL do GitHub</h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('github_url_options_group');
+                do_settings_sections('github-url-settings');
+                ?>
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row">URL Salva</th>
+                        <td><?php echo esc_attr(get_option('github_url')); ?></td>
+                    </tr>
+                </table>
+                <?php submit_button(); ?>
+            </form>
         </div>
 <?php
+    }
+
+    // função para registrar as opções do github
+    public function register_settings()
+    {
+        register_setting('github_url_options_group', 'github_url', 'esc_url_raw');
+        add_settings_section('github_url_section', 'Configurações do GitHub', null, 'github-url-settings');
+        add_settings_field(
+            'github_url_field',
+            'URL do GitHub',
+            [$this, 'render_github_url_field'],
+            'github-url-settings',
+            'github_url_section'
+        );
+    }
+    public function render_github_url_field()
+    {
+        echo '<input type="url" name="github_url" value="' . esc_attr(get_option('github_url')) . '" class="regular-text" />';
     }
 }
 
