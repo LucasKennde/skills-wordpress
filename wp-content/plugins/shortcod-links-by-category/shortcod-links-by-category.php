f52064139ced20558bc7d1b43d250de5482cae76
Lucas Kennde <lucaskennde@gmail.com>
2024-12-02 16:42:38 -0300

    Created path shortcod link by category

---
+++ wp-content/plugins/shortcod-links-by-category/shortcod-links-by-category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Shortcod Links By Category
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     shortcod-links-by-category
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Shortcod_Links_By_Category
+ */
+
+// Your code starts here.
---
+++ wp-content/plugins/shortcod-links-by-category/tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Shortcod_Links_By_Category
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
