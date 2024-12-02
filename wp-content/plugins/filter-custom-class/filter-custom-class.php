0ae8909f773c512be0969ccf3384e958f5f75950
Lucas Kennde <lucaskennde@gmail.com>
2024-12-02 16:32:50 -0300

    finaly first plugin

--- wp-content/plugins/filter-custom-class/filter-custom-class.php
+++ wp-content/plugins/filter-custom-class/filter-custom-class.php
@@ -1,15 +1,32 @@
 <?php
+
 /**
  * Plugin Name:     Filter Custom Class
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/lucaskennde/skills-wordpress
+ * Description:     update to all tags <p> and add custom class
+ * Author:          LUCAS KENNDE ARAÚJO DE MOURA
+ * Author URI:      https://github.com/lucaskennde
  * Text Domain:     filter-custom-class
  * Domain Path:     /languages
  * Version:         0.1.0
- *
+ * Requires PHP:    7.4
  * @package         Filter_Custom_Class
  */
 
-// Your code starts here.
+class addCustomClassToTagParagraphs
+{
+    private $custom_class;
+
+    public function __construct($custom_name  = 'custom-class')
+    {
+        $this->custom_class = $custom_name;
+        add_filter('the_content', [$this, 'add_custom_class']);
+    }
+
+    public function add_custom_class($content)
+    {
+        return str_replace('<p>', '<p class="' . esc_attr($this->custom_class) . '">', $content);
+    }
+}
+
+new addCustomClassToTagParagraphs();
