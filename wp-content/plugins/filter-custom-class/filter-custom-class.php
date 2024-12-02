9185576e216966a66a351c6ba0a7b084e928baac
Lucas Kennde <lucaskennde@gmail.com>
2024-12-02 16:00:55 -0300

    Created first plugin with wp-cli

---
+++ wp-content/plugins/filter-custom-class/filter-custom-class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Filter Custom Class
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     filter-custom-class
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Filter_Custom_Class
+ */
+
+// Your code starts here.
